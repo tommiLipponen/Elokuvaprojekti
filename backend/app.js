226ce9433a2b75e4a86274fa8da0ebdaded2e650
Topi Pietilänaho <topi.pietilanaho99@gmail.com>
2026-09-09 20:31:43 +0300

    Swagger testing done to Creater User

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 const authRoutes = require('./modules/auth/auth.routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/auth', authRoutes);
 
